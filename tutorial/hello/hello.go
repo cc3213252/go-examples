90797c1f27085009021afa2cd5746721744180de
yudan.chen <cc3213252@163.com>
2021-07-26 14:23:47 +0800

    异常处理

--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"example.com/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	// 禁止打印时间、源文件、行数
+	log.SetFlags(0)
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
