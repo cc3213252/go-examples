bd020248103ff22bcc0bcd92b7919cb8e6fccbac
yudan.chen <cc3213252@163.com>
2021-07-27 13:26:02 +0800

    在线wiki，展示与编辑

--- gowiki/README.md
+++ gowiki/README.md
@@ -4,3 +4,12 @@
 go build wiki.go
 ./wiki
 ```
+
+## net/http简单实用
+
+ListenAndServe这个函数正常会阻塞，不阻塞了返回错误  
+![net例子](https://github.com/cc3213252/go-examples/raw/master/img/net-http-example.png)
+
+## 升级后wiki
+
+http://localhost:8080/view/TestPage
---
+++ gowiki/simple-net.go
@@ -0,0 +1,20 @@
+/**
+@author: yudan.chen
+@date: 2021/7/27
+**/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
